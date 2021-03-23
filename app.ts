86c26d65f266fa159de28bb0c4fd2a8ce9cf3d7c
koreanwglasses <koreanwglasses@gmail.com>
2021-03-23 13:28:12 -0400

    communication with backend proof of concept

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
 import * as express from "express";
+import * as cors from "cors";
 import { resolve } from "path";
-import { mode } from "./config";
 
 // Create the express application
 const app = express();
+
+app.use(cors())
+
+app.get("/api/test", (_, response) => {
+  response.status(200).json({message: "hello world! :3"})
+})
 
 // Declare the path to frontend's static assets
 app.use(express.static(resolve("../frontend/static")));
@@ -12,9 +18,7 @@ app.use(express.static(resolve("../frontend/static")));
 app.get("*", (_, response) => {
   response.sendFile(
     resolve(
-      "../frontend",
-      mode === "production" ? "build" : "dist",
-      "index.html"
+      "../frontend/build/index.html"
     )
   );
 });
